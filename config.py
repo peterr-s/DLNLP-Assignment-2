88f63319298acca952128e1e00c1f44441a03ca9
Peter Schoener <peter.c.schoener@gmail.com>
2017-07-31 10:37:05 +0200

    fixed hyperparameters

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class DefaultConfig:
 	#input_dropout = 0.95
 	#hidden_dropout = 0.95
 	max_timesteps = 20
-	LSTM_ct = 2
-	LSTM_sz = 100
-	embedding_sz = 50
+	LSTM_ct = 4
+	LSTM_sz = 200
+	embedding_sz = 100
 	dropout_ratio = 0.9
